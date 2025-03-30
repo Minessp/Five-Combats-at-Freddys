806cc090eeb7f20420ac9892e47f56fa0ad8123a
Minessp <minestadionchannel@gmail.com>
2025-03-30 18:20:25 -0300

    Adicionado inimigo ao cenário do jogo

---
+++ src/main/java/org/msp/game/model/Enemies.java
@@ -0,0 +1,65 @@
+package org.msp.game.model;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Enemies {
+    private Image image;
+    private int x, y;
+    private int height, width;
+    private boolean isVisible;
+
+    // private static final int LARGURA = 1558;
+    private static int VELOCIDADE = 10;
+
+    public Enemies(int x, int y) {
+        this.x = x;
+        this.y = y;
+        isVisible = true;
+    }
+
+    // Recebe a imagem do attack
+    public void load() {
+        ImageIcon ref = new ImageIcon("src//images//enemies//purpleguy1.png");
+        image = ref.getImage();
+
+        this.width = image.getWidth(null);
+        this.height = image.getHeight(null);
+    }
+
+    // Define a retirada do objeto da tela
+    public void update() {
+        this.x -= VELOCIDADE;
+        //if (this.x > LARGURA) {
+        //    isVisible = false;
+        //}
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
+
+    public boolean isVisible() {
+        return isVisible;
+    }
+
+    public void setVisible(boolean visible) {
+        isVisible = visible;
+    }
+
+    public static int getVELOCIDADE() {
+        return VELOCIDADE;
+    }
+
+    public static void setVELOCIDADE(int VELOCIDADE) {
+        Enemies.VELOCIDADE = VELOCIDADE;
+    }
+}
--- src/main/java/org/msp/game/model/GameplayPhase.java
+++ src/main/java/org/msp/game/model/GameplayPhase.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyAdapter;
 import java.awt.event.KeyEvent;
+import java.util.ArrayList;
 import java.util.List;
 
 public class GameplayPhase extends JPanel implements ActionListener {
@@ -13,6 +14,7 @@ public class GameplayPhase extends JPanel implements ActionListener {
     private Image background;
     private Player player;
     private Timer timer;
+    private List<Enemies> enemies;
 
     public GameplayPhase() {
         setFocusable(true);
@@ -28,6 +30,19 @@ public class GameplayPhase extends JPanel implements ActionListener {
 
         timer = new Timer(1, this);
         timer.start();
+
+        enimiesInicialize();
+    }
+    // Cria inimigos em posições de altura e largura randomicas
+    public void enimiesInicialize() {
+        int coordenades[] = new int[60];
+        enemies = new ArrayList<Enemies>();
+
+        for (int i = 0; i < coordenades.length; i++) {
+            int x = (int)(Math.random() * 8000+1600);
+            int y = (int)(Math.random() * 650+30);
+            enemies.add(new Enemies(x, y));
+        }
     }
 
     // Renderiza os elementos do jogo na tela
@@ -43,6 +58,13 @@ public class GameplayPhase extends JPanel implements ActionListener {
             a.load();
             graphics.drawImage(a.getImage(), a.getX(), a.getY(), this);
         }
+
+        for (int j = 0; j < enemies.size(); j++){
+            Enemies in = enemies.get(j);
+            in.load();
+            graphics.drawImage(in.getImage(), in.getX(), in.getY(), this);
+        }
+
         g.dispose();
     }
 
@@ -57,6 +79,15 @@ public class GameplayPhase extends JPanel implements ActionListener {
                 a.update();
             } else {
                 attack.remove(i);
+            }
+        }
+
+        for (int j = 0; j < enemies.size(); j++){
+            Enemies in = enemies.get(j);
+            if(in.isVisible()){
+                in.update();
+            } else {
+                enemies.remove(j);
             }
         }
 
