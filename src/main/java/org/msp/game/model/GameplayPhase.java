ceee98be881831d96aa2aff194505d7dcbbe6665
Minessp <minestadionchannel@gmail.com>
2025-03-30 14:54:36 -0300

    Adicionado o Freddy Fazbear e sua movimentação

--- src/main/java/org/msp/game/model/GameplayPhase.java
+++ src/main/java/org/msp/game/model/GameplayPhase.java
@@ -2,18 +2,56 @@ package org.msp.game.model;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 
-public class GameplayPhase extends JPanel {
+public class GameplayPhase extends JPanel implements ActionListener {
+
     private Image background;
+    private Player player;
+    private Timer timer;
 
     public GameplayPhase() {
-        ImageIcon ref = new ImageIcon("src//images//background.png");
+        setFocusable(true);
+        setDoubleBuffered(true);
+
+        ImageIcon ref = new ImageIcon("src//images//background//background.png");
         background = ref.getImage();
+
+        player = new Player();
+        player.load();
+
+        addKeyListener(new KeyboardAdapter());
+
+        timer = new Timer(1, this);
+        timer.start();
     }
 
+    // Renderiza os elementos do jogo na tela
     public void paint(Graphics g) {
         Graphics2D graphics = (Graphics2D) g;
         graphics.drawImage(background, 0, 0, null);
+        graphics.drawImage(player.getImage(), player.getX(), player.getY(), null);
         g.dispose();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        player.update();
+        repaint();
+    }
+
+    private class KeyboardAdapter extends KeyAdapter {
+        @Override
+        public void keyPressed(KeyEvent e) {
+            player.keyPressed(e);
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            player.keyRealise(e);
+        }
     }
 }
--- src/main/java/org/msp/game/model/Player.java
+++ src/main/java/org/msp/game/model/Player.java
@@ -1,6 +1,8 @@
 package org.msp.game.model;
 
+import javax.swing.*;
 import java.awt.*;
+import java.awt.event.KeyEvent;
 
 public class Player {
     private int x, y;
@@ -8,4 +10,77 @@ public class Player {
     private Image image;
     private int width, height;
 
+    // Posição inicial do player
+    public Player() {
+        this.x = 100;
+        this.y = 100;
+    }
+
+    // Carrega a imagem do personagem
+    public void load() {
+        ImageIcon ref = new ImageIcon("src//images//character//freddy_right.png");
+        image = ref.getImage();
+        height = image.getHeight(null);
+        width = image.getWidth(null);
+    }
+
+    // Atualiza a posição do personagem
+    public void update() {
+        x += dx;
+        y += dy;
+    }
+
+    // Recebe tecla apertada e atualiza o valor do personagem
+    public void keyPressed(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
+            dy = -10;
+        }
+
+        if (code == KeyEvent.VK_S) {
+            dy = 10;
+        }
+
+        if (code == KeyEvent.VK_A) {
+            dx = -10;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            dx = 10;
+        }
+    }
+
+    // Para de atualizar a posição do personagem
+    public void keyRealise(KeyEvent e) {
+        int code = e.getKeyCode();
+
+        if (code == KeyEvent.VK_W) {
+            dy = 0;
+        }
+
+        if (code == KeyEvent.VK_S) {
+            dy = 0;
+        }
+
+        if (code == KeyEvent.VK_A) {
+            dx = 0;
+        }
+
+        if (code == KeyEvent.VK_D) {
+            dx = 0;
+        }
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public Image getImage() {
+        return image;
+    }
 }
